838ce723000ea5d09312695245bb6ca53c51a531
TheHawk <patricklehloenya2@gmail.com>
2024-12-31 00:08:12 +0200

    feat: can process nested html tags

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -11,16 +11,54 @@ class HTMLNode:
 
     def props_to_html(self):
         if self.props == None:
-            raise Exception("No html attributes provided")
+            return ""
         attributes = ""
         for prop in self.props:
             value = self.props[prop]
-            attributes += f' {prop}="{self.props[prop]}" '
-
+            attributes += f' {prop}="{self.props[prop]}"'
         return attributes
 
     def __repr__(self):
         return f"{self.__class__.__name__}({self.tag}, {self.value}, {self.children}, {self.props})"
+
+
+
+class LeafNode(HTMLNode):
+
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag,value,props=props)
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError("Leaf node must have a value")
+        if self.tag == None:
+            return self.value
+        htmltag = f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+        return htmltag
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.tag}, {self.value}, {self.props})"
+
+class ParentNode(HTMLNode):
+
+    def __init__(self,tag, children, props=None):
+        super().__init__(tag,children=children,props=props)
+
+    def to_html(self):
+        if self.tag == None:
+            raise ValueError("HTML tag is missing")
+        if self.children == None:
+            raise ValueError("Inline tags missing")
+        htmltag = f"<{self.tag}{self.props_to_html()}>{"".join(list(map(lambda x: x.to_html(),self.children)))}</{self.tag}>"
+        return htmltag
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.tag}, {self.value}, {self.props})"
+
+
+
+
 
 
 
